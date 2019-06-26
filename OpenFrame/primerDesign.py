aef750eb55cd29acef9a3b5ae6bc2e9eefc82f9b
Cooper Hopkin <coop.hopkin@gmail.com>
2019-06-26 14:19:51 -0600

    changed primerDesign for debug

--- OpenFrame/primerDesign.py
+++ OpenFrame/primerDesign.py
@@ -16,10 +16,15 @@ from tkinter import filedialog
 if __name__ == '__main__':
     #TODO: allow for command line input (or data file)
 
-    
-
     #cDNA file
-    cdnaFile = filedialog.askopenfilename(title="cDNA File", filetypes=())
+    cdnaFile = str(input("Enter the cDNA filename: "))
 
     #vector file
-    vectorFile = filedialog.askopenfile(title='Vector File', filetypes=())
+    vectorFile = str(input("Enter the cDNA filename: "))
+
+    i = 1
+    for frame in openFrame.findFrames(100, dataFile=cdnaFile):
+        print(f'{i}:\tcDNA:\t\t{frame[0][:30]}...{frame[0][-15:]}')
+        print(f'\tProtSeq:\t{frame[1][:30]}...{frame[1][-15:]}')
+        print(f'\tStart: {frame[2]+1}\t\tEnd: {frame[3]+1}\t\tLength: {frame[4]}')
+        i += 1
