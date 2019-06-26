4dbd33c9186d09195c263f3684c558311ecdb6e3
Cooper Hopkin <coop.hopkin@gmail.com>
2019-06-26 14:07:16 -0600

    added a R enzyme database

--- OpenFrame/primerDesign.py
+++ OpenFrame/primerDesign.py
@@ -1,7 +1,25 @@
 # primerDesign.py
 '''
+Allow user to pick a cDNA sequence, vector, find the open frame, decide which 
+restriction enzyme to use, and finally design the primer
+
 Author: John Cooper Hopkin
 Git Repo: coopman66/BioPython
 6-25-2019
 Version: 0.1.0
 '''
+
+import openFrame
+import sqlite3
+from tkinter import filedialog
+
+if __name__ == '__main__':
+    #TODO: allow for command line input (or data file)
+
+    
+
+    #cDNA file
+    cdnaFile = filedialog.askopenfilename(title="cDNA File", filetypes=())
+
+    #vector file
+    vectorFile = filedialog.askopenfile(title='Vector File', filetypes=())
