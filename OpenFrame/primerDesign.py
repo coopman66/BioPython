3a460b20779286430bf9c74d25cf71062919b5a5
Cooper Hopkin <coop.hopkin@gmail.com>
2019-06-26 16:39:31 -0600

    Finished cleaning pet32 gb TODO add rules for parsing

--- OpenFrame/primerDesign.py
+++ OpenFrame/primerDesign.py
@@ -12,6 +12,9 @@ Version: 0.1.0
 import openFrame
 import sqlite3
 from tkinter import filedialog
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
 
 if __name__ == '__main__':
     #TODO: allow for command line input (or data file)
@@ -22,9 +25,11 @@ if __name__ == '__main__':
     #vector file
     vectorFile = str(input("Enter the cDNA filename: "))
 
-    i = 1
+    '''i = 1
     for frame in openFrame.findFrames(100, dataFile=cdnaFile):
         print(f'{i}:\tcDNA:\t\t{frame[0][:30]}...{frame[0][-15:]}')
         print(f'\tProtSeq:\t{frame[1][:30]}...{frame[1][-15:]}')
         print(f'\tStart: {frame[2]+1}\t\tEnd: {frame[3]+1}\t\tLength: {frame[4]}')
-        i += 1
+        i += 1'''
+
+    vector = SeqIO.read(vectorFile, 'genbank')
